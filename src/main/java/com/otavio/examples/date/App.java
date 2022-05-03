02db2e89851fa572f2707c6f92e5f1115a86c325
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-03 12:14:35 +0100

    Update entities to cover all date types
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otavio/examples/date/App.java
+++ src/main/java/com/otavio/examples/date/App.java
@@ -1,13 +1,43 @@
 package com.otavio.examples.date;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.time.Clock;
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.MonthDay;
+import java.time.OffsetDateTime;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.ZonedDateTime;
+
+public class App {
+    public static void main(String[] args) {
+
+        DayOfWeek dayOfWeek = DayOfWeek.MONDAY;
+        Month month = Month.JANUARY;
+        MonthDay monthDay = MonthDay.now();
+        YearMonth yearMonth = YearMonth.now();
+        Year year = Year.now();
+        LocalDate localDate = LocalDate.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        OffsetDateTime offsetDateTime = OffsetDateTime.now();
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        Clock clock = Clock.systemUTC();
+        Instant instant = Instant.now();
+
+        System.out.println("DayOfWeek: " + dayOfWeek);
+        System.out.println("month: " + month);
+        System.out.println("MonthDay: " + monthDay);
+        System.out.println("YearMonth: " + yearMonth);
+        System.out.println("Year: " + year);
+        System.out.println("LocalDate: " + localDate);
+        System.out.println("LocalDateTime: " + localDateTime);
+        System.out.println("OffsetDateTime: " + offsetDateTime);
+        System.out.println("ZonedDateTime: " + zonedDateTime);
+        System.out.println("Clock: " + clock.getZone());
+        System.out.println("Instant: " + instant);
+
     }
 }
