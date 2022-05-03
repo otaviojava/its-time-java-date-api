a70fa4ecebd94f8390aa4b78c2c74e96a7ad8926
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-03 14:12:32 +0100

    Add compare method
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/com/otavio/examples/date/TimeOperationTest.java
+++ src/test/java/com/otavio/examples/date/TimeOperationTest.java
@@ -3,6 +3,7 @@ package com.otavio.examples.date;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.Month;
@@ -12,7 +13,7 @@ import java.time.temporal.TemporalAdjusters;
 public class TimeOperationTest {
 
     @Test
-    public void shouldSumDay() {
+    public void shouldOperate() {
         LocalDate myBirthday = Year.of(1988).atMonth(Month.JANUARY).atDay(9);
         LocalDate yesterday = myBirthday.minusDays(1);
         LocalDate oneYear = myBirthday.plusDays(365);
@@ -21,13 +22,22 @@ public class TimeOperationTest {
     }
 
     @Test
-    public void shouldCreateLastDay() {
+    public void shouldCompare() {
+        LocalDate myBirthday = Year.of(1988).atMonth(Month.JANUARY).atDay(9);
+        LocalDate now = LocalDate.now();
+        Assertions.assertTrue(now.isAfter(myBirthday));
+        Assertions.assertFalse(now.isBefore(myBirthday));
+    }
+
+    @Test
+    public void shouldUseTemporalAdjusters() {
         LocalDate myBirthday = Year.of(1988).atMonth(Month.JANUARY).atDay(9);
         LocalDate newYear = myBirthday.with(TemporalAdjusters.firstDayOfMonth());
         LocalDate lastDayOfMonth = myBirthday.with(TemporalAdjusters.lastDayOfMonth());
-
+        LocalDate nextMonday = myBirthday.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
         Assertions.assertEquals(Year.of(1988).atMonth(Month.JANUARY).atDay(1), newYear);
         Assertions.assertEquals(Year.of(1988).atMonth(Month.JANUARY).atDay(31), lastDayOfMonth);
+        Assertions.assertEquals(Year.of(1988).atMonth(Month.JANUARY).atDay(11), nextMonday);
 
     }
 }
