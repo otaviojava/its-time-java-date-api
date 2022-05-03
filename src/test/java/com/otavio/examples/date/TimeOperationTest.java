b9e6d892441548818c80e5039aacd2b89a8dbedf
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-03 16:42:26 +0100

    Add timeoperation test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/com/otavio/examples/date/TimeOperationTest.java
+++ src/test/java/com/otavio/examples/date/TimeOperationTest.java
@@ -9,11 +9,15 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.OffsetTime;
+import java.time.Period;
 import java.time.Year;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.TemporalQuery;
 
 public class TimeOperationTest {
 
@@ -59,14 +63,46 @@ public class TimeOperationTest {
         ZoneId saoPaulo = ZoneId.of("America/Sao_Paulo");
         ZoneId portugal = ZoneId.of("Portugal");
         LocalDateTime timeSP = Year.of(2021).atMonth(Month.MAY).atDay(3)
-                .atTime(12,0,0);
+                .atTime(12, 0, 0);
 
         LocalDateTime timeLisbon = Year.of(2021).atMonth(Month.MAY).atDay(3)
-                .atTime(16,0,0);
+                .atTime(16, 0, 0);
 
         ZonedDateTime zoneSaoPaulo = ZonedDateTime.of(timeSP, saoPaulo);
         ZonedDateTime zoneLisbon = ZonedDateTime.of(timeLisbon, portugal);
         Assertions.assertTrue(zoneSaoPaulo.isEqual(zoneLisbon));
     }
 
+    @Test
+    public void shouldUseChronoUnit() {
+        LocalDate today = LocalDate.now();
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        Assertions.assertEquals(1, ChronoUnit.DAYS.between(today, tomorrow));
+    }
+
+    @Test
+    public void shouldUsePeriod() {
+        LocalDate dateA = LocalDate.of(2012, Month.APRIL, 7);
+        LocalDate dateB = LocalDate.of(2015, Month.DECEMBER, 5);
+
+        Period period = Period.between(dateA, dateB);
+
+        Assertions.assertEquals(3, period.getYears());
+        Assertions.assertEquals(7, period.getMonths());
+        Assertions.assertEquals(28, period.getDays());
+        Assertions.assertEquals(43, period.toTotalMonths());
+    }
+
+    @Test
+    public void shouldIsWeekend() {
+        TemporalQuery<Boolean> weekend = temporal -> {
+            int dayOfWeek = temporal.get(ChronoField.DAY_OF_WEEK);
+            return dayOfWeek == DayOfWeek.SATURDAY.getValue()
+                    || dayOfWeek == DayOfWeek.SUNDAY.getValue();
+        };
+        LocalDate date = LocalDate.of(2018, 5, 4);
+        LocalDateTime sunday = LocalDateTime.of(2018, 5, 6, 17, 0);
+        Assertions.assertFalse(date.query(weekend));
+        Assertions.assertTrue(sunday.query(weekend));
+    }
 }
